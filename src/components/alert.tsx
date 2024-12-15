708184ee85766ed4a5b4f11cc77318eed0c149aa
snrakshith <snrakshith.97@gmail.com>
2024-12-15 18:14:03 +0530

    chore: initial setup

---
+++ src/components/alert.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Alert, Button } from "rizzui";
+
+export default function AlertClearable() {
+  const [state, setState] = React.useState(true);
+  return (
+    <>
+      <Button onClick={() => setState(true)} className="tracking-wider">
+        Info Alert
+      </Button>
+      {state && (
+        <Alert
+          color="info"
+          variant="flat"
+          closable
+          onClose={() => setState(false)}
+        >
+          <p className="font-semibold">Alert with info</p>
+          <p>
+            Attention All! We are excited to announce the launch of our new
+            product/service.
+          </p>
+        </Alert>
+      )}
+    </>
+  );
+}
